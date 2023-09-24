8c68a0aa6487df9721e15e64628a822ce6fe5070
laulelien <aurelien.dlry@gmail.com>
2023-09-24 16:37:38 +0200

    4 qu done

--- src/mem_internals.c
+++ src/mem_internals.c
@@ -17,49 +17,62 @@ unsigned long knuth_mmix_one_round(unsigned long in)
 
 void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k)
 {
-    /* ecrire votre code ici */
-    return (void *)0;
+    uint64_t taille = (uint64_t)size;
+    *(uint64_t *)ptr = taille;
+    *(uint64_t *)(ptr + 24 + size) = taille;
+    uint64_t magic = ((uint64_t)knuth_mmix_one_round((unsigned long)ptr) & (~(0b11UL))) + (uint64_t)k;
+    *(uint64_t *)(ptr + 8) = magic;
+    *(uint64_t *)(ptr + 16 + size) = magic;
+
+    return ptr + 16;
 }
 
-Alloc
-mark_check_and_get_alloc(void *ptr)
+Alloc mark_check_and_get_alloc(void *ptr)
 {
-    /* ecrire votre code ici */
     Alloc a = {};
+    a.ptr = ptr - 16;
+    uint64_t taille = *(uint64_t *)(a.ptr);
+    a.size = taille;
+    uint64_t magic = *(uint64_t *)(ptr - 8);
+    a.kind = (MemKind)(magic & 0x00000003);
+    assert(magic == (uint64_t)knuth_mmix_one_round(((unsigned long)a.ptr) & (~(0b11UL))) + (uint64_t)a.kind);
+    assert(taille == *(uint64_t *)(ptr + 8 + a.size));
+    assert(magic == *(uint64_t *)(ptr + a.size));
     return a;
 }
 
-
 unsigned long
-mem_realloc_small() {
+mem_realloc_small()
+{
     assert(arena.chunkpool == 0);
     unsigned long size = (FIRST_ALLOC_SMALL << arena.small_next_exponant);
     arena.chunkpool = mmap(0,
-			   size,
-			   PROT_READ | PROT_WRITE | PROT_EXEC,
-			   MAP_PRIVATE | MAP_ANONYMOUS,
-			   -1,
-			   0);
+                           size,
+                           PROT_READ | PROT_WRITE | PROT_EXEC,
+                           MAP_PRIVATE | MAP_ANONYMOUS,
+                           -1,
+                           0);
     if (arena.chunkpool == MAP_FAILED)
-	handle_fatalError("small realloc");
+        handle_fatalError("small realloc");
     arena.small_next_exponant++;
     return size;
 }
 
 unsigned long
-mem_realloc_medium() {
+mem_realloc_medium()
+{
     uint32_t indice = FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant;
     assert(arena.TZL[indice] == 0);
     unsigned long size = (FIRST_ALLOC_MEDIUM << arena.medium_next_exponant);
-    assert( size == (1UL << indice));
+    assert(size == (1UL << indice));
     arena.TZL[indice] = mmap(0,
-			     size*2, // twice the size to allign
-			     PROT_READ | PROT_WRITE | PROT_EXEC,
-			     MAP_PRIVATE | MAP_ANONYMOUS,
-			     -1,
-			     0);
+                             size * 2, // twice the size to allign
+                             PROT_READ | PROT_WRITE | PROT_EXEC,
+                             MAP_PRIVATE | MAP_ANONYMOUS,
+                             -1,
+                             0);
     if (arena.TZL[indice] == MAP_FAILED)
-	handle_fatalError("medium realloc");
+        handle_fatalError("medium realloc");
     // align allocation to a multiple of the size
     // for buddy algo
     arena.TZL[indice] += (size - (((intptr_t)arena.TZL[indice]) % size));
@@ -67,15 +80,15 @@ mem_realloc_medium() {
     return size; // lie on allocation size, but never free
 }
 
-
 // used for test in buddy algo
 unsigned int
-nb_TZL_entries() {
+nb_TZL_entries()
+{
     int nb = 0;
-    
-    for(int i=0; i < TZL_SIZE; i++)
-	if ( arena.TZL[i] )
-	    nb ++;
+
+    for (int i = 0; i < TZL_SIZE; i++)
+        if (arena.TZL[i])
+            nb++;
 
     return nb;
 }
--- src/mem_small.c
+++ src/mem_small.c
@@ -8,13 +8,43 @@
 #include "mem.h"
 #include "mem_internals.h"
 
+typedef struct _node {
+    void* ptr;
+    struct _node* next;
+} node;
+
 void *
 emalloc_small(unsigned long size)
 {
-    /* ecrire votre code ici */
-    return (void *)0;
+    if (arena.chunkpool == NULL) {
+        unsigned long size = mem_realloc_small();
+        void* current_addr = arena.chunkpool;
+        arena.chunkpool = malloc(sizeof(node));
+        node* current_node = arena.chunkpool;
+        current_node->ptr = current_addr;
+        current_addr += CHUNKSIZE;
+        size -= CHUNKSIZE;
+        while (size >= CHUNKSIZE) {
+            current_node->next = malloc(sizeof(node));
+            current_node->ptr = current_addr;
+            current_addr += CHUNKSIZE;
+            size -= CHUNKSIZE;
+            current_node = current_node->next;
+        }
+    }
+
+    void* chunk_addr = ((node*) arena.chunkpool)->ptr;
+    node* to_delete = arena.chunkpool;
+    arena.chunkpool = ((node*) arena.chunkpool)->next;
+    free(to_delete);
+    
+    return mark_memarea_and_get_user_ptr(chunk_addr, size, SMALL_KIND);
 }
 
 void efree_small(Alloc a) {
-    /* ecrire votre code ici */
+    assert(a.kind == SMALL_KIND);
+    node* new_node = malloc(sizeof(node));
+    new_node->ptr = a.ptr;
+    new_node->next = arena.chunkpool;
+    arena.chunkpool = new_node;
 }
